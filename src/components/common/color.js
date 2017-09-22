dcfa315b06a26dc672074c99fbc3236bdd770d34
MurrayLaHood-Burns <murray.lahoodburns@gmail.com>
2017-09-22 11:39:39 -0600

    basic setup

---
+++ src/components/common/color.js
@@ -0,0 +1,14 @@
+const colors = {
+  text: '#424242',
+  link: '#1E88E5',
+  green: '#43A047',
+  teal: '#00897B',
+  blue: '#1E88E5',
+  blueDark: '#1976D2',
+  red: '#E53935',
+  purple: '#8E24AA',
+  navy: '#071D49',
+  gray: '#757575'
+};
+
+export {colors};
---
+++ src/components/common/shadows.js
@@ -0,0 +1,11 @@
+const shadows = {
+  s1: '0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)',
+  s2: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
+  s3: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
+  s4: '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)',
+  s5: '0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22)',
+  hover: '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)',
+  transition: 'all 0.3s cubic-bezier(.25,.8,.25,1)'
+};
+
+export {shadows};
